c1e153157a5b6ad97474a1b4f0ac41ee098e8280
kittykatt210 <keperry30@icloud.com>
2023-11-17 16:55:51 -0600

    Created tag routes.

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Product, Category, Tag, ProductTag, Product } = require('../../models');
+const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
 
@@ -7,7 +7,7 @@ const { Product, Category, Tag, ProductTag, Product } = require('../../models');
 router.get('/', async (req, res) => {
   try {
     const productData = await Product.findAll({
-      include: [{ model: Category }, { model: ProductTag }, { model: Tag }],
+      include: [{ model: Category }, { model: Tag }],
     });
     res.status(200).json(productData);
   } catch (err) {
@@ -19,7 +19,7 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   try {
     const productData = await Product.findByPk(req.params.id, {
-      include: [{ model: Category }, { model: ProductTag }, { model: Tag }],
+      include: [{ model: Category }, { model: Tag, though: ProductTag, as: 'product_tags' }],
     });
     if (!productData) {
       res.status(404).json({ message: 'No product found with that id!' });
